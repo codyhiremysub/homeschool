6ac88b5b553045c207559556ad6ef83fd87fe2a4
Cody Lillywhite <cody@hiremysub.com>
2023-11-14 08:59:46 -0700

    feat: updated login

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,20 +1,49 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { PulseLoader } from "react-spinners";
 import { useDispatch } from "react-redux";
 import { Formik, Form, Field } from "formik";
-import { SubmitButton } from "../../components/Styled";
+import { CheckIcon } from "@heroicons/react/24/outline";
+import { MdWarning } from "react-icons/md";
+import { openInNewTab } from "../../utils/helpers/navigation";
+import * as Styled from "../../components/Styled";
 import { validateEmail, validatePassword } from "../../utils/helpers/input";
 import InputField from "../../components/input_fields/InputField";
 import { login } from "../../utils/requests/auth-requests";
 import { successToast } from "../../utils/helpers/toasts";
 import { login as loginAction } from "../../redux/reducers/auth-reducer";
+import Modal from "../../components/Modal";
+import authorization from "../../api/authorization";
+import axios from "../../api/config";
+import ModalTitle from "../../components/dashboard/ModalTitle";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [submitError, setSubmitError] = useState("");
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const token = query.get("token");
+  const [showVerifiedModal, setShowVerifiedModal] = useState(false);
+  const [error, setError] = useState(false);
+
+  const activateAccountHandler = async () => {
+    try {
+      await axios.post("auth/activate-account", {}, authorization(token));
+
+      setShowVerifiedModal(true);
+    } catch (error) {
+      setError(error.response.data.error);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      activateAccountHandler();
+    }
+  }, [token]);
 
   return (
     <div className="flex min-h-full flex-1">
@@ -44,6 +73,7 @@ const LoginPage = () => {
                   const results = await login(values);
                   dispatch(
                     loginAction({
+                      email: values.email,
                       userId: results.user_id,
                     })
                   );
@@ -83,7 +113,7 @@ const LoginPage = () => {
                     </Link>
                   </div>
 
-                  <SubmitButton
+                  <Styled.SubmitButton
                     type="submit"
                     className="w-full text-center font-medium"
                     $proceedAllowed={isValid && dirty}
@@ -94,10 +124,29 @@ const LoginPage = () => {
                     ) : (
                       "Login"
                     )}
-                  </SubmitButton>
+                  </Styled.SubmitButton>
                 </Form>
               )}
             </Formik>
+
+            <div className="mb-7 mt-3.5">
+              <span className="text-sm font-medium text-black">
+                Don't have an account?
+              </span>
+              <button
+                type="button"
+                title="Sign Up"
+                onClick={() => openInNewTab("https://hiremysub.com/join")}
+                className="ml-2 text-sm font-medium text-blue-500 hover:underline"
+              >
+                Sign up
+              </button>
+            </div>
+
+            <p className="text-center text-sm font-medium text-black">
+              &#169; HireMySub.com {new Date().getFullYear()} - All Rights
+              Reserved
+            </p>
           </div>
         </div>
       </div>
@@ -108,6 +157,59 @@ const LoginPage = () => {
           alt=""
         />
       </div>
+      {showVerifiedModal && "verified"}
+      <Modal
+        show={showVerifiedModal}
+        onClose={() => setShowVerifiedModal(false)}
+      >
+        <ModalTitle
+          title="Account Activated"
+          Icon={CheckIcon}
+          iconColor="green"
+        />
+        <p className="text-center">
+          Your account has been activated. You may now proceed to login.
+        </p>
+        <div className="mt-5 flex w-full justify-center">
+          <Styled.PrimaryFilledButton
+            className="col-start-2 w-full xs:w-64"
+            onClick={() => {
+              setShowVerifiedModal(false);
+              navigate("..", { relative: "path" });
+            }}
+          >
+            Continue
+          </Styled.PrimaryFilledButton>
+        </div>
+      </Modal>
+      <Modal
+        show={!!error}
+        onClose={() => {
+          setError(false);
+          navigate("..", { relative: "path" });
+        }}
+      >
+        <ModalTitle
+          title="Error Activating Account"
+          Icon={MdWarning}
+          iconColor="red"
+        />
+        <p className="text-center">
+          Link is expired or invalid. Request a new email and try again.
+        </p>
+        <div className="mt-5 flex w-full justify-center">
+          <Styled.PrimaryFilledButton
+            className="col-start-2 w-full xs:w-64"
+            onClick={async () => {
+              setError(false);
+              navigate("..", { relative: "path" });
+              await activateAccountHandler();
+            }}
+          >
+            Resend Email
+          </Styled.PrimaryFilledButton>
+        </div>
+      </Modal>
     </div>
   );
 };
