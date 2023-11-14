c208d1cd0e2ebc53f6706ac066e7f6ffbab1b620
Cody Lillywhite <cody@hiremysub.com>
2023-11-14 08:54:17 -0700

    chore: rq

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import { persistor, store } from "./redux/store";
 
 const container = document.getElementById("homeschool-root");
 const root = createRoot(container);
+const queryClient = new QueryClient();
 
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
+    <QueryClientProvider client={queryClient}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </QueryClientProvider>
   </Provider>
 );
